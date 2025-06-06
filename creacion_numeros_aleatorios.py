6027083be6e8d977ee8fcb6f332769a72ed32f88
Liz-Elizabeth <el.reyess@duocuc.cl>
2025-06-06 11:04:29 -0400

    version 3
    ciclos

---
+++ creacion_numeros_aleatorios.py
@@ -0,0 +1,18 @@
+
+import random #libreria para llamar y crear numeros random
+numero = random.randint(1,100)#rango para el numero random quiero que la variable numer me guarde un numero de uno a cien
+print(numero)
+
+numeros = [] #coleccion vacia
+
+for i in range(100): #Ciclos para repetir 100 veces las iteraciones
+   
+    numeros.apend(random.randint) #guardamos en la lista en el numero aleatorio
+
+    print(numeros) #visualizamos lista
+
+
+
+
+
+
